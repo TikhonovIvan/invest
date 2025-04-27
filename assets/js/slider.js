303316ea4a51496cc88e10b74698ab00e114c097
Digidon727 <ivntik77@gmail.com>
2025-04-27 23:23:07 +0600

    commit-7

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,19 +1,61 @@
 const swiper = new Swiper('.expert-swiper', {
-    slidesPerView: 1.2,
+    slidesPerView: 1.4,
     spaceBetween: 10,
+   
     breakpoints: {
+
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 5,
+        
+    
+      },
+      425: {
+        slidesPerView: 1,
+        
+      },
+
       768: {
-        slidesPerView: 2.2,
+        slidesPerView: 2,
       },
       1024: {
-        slidesPerView: 3.2,
+        slidesPerView: 3,
       },
-      1280: {
+      
+      1380: {
         slidesPerView: 4,
       }
     },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+  });
+
+
+// Отзывы слайдер
+
+  const reviewsSwiper = new Swiper('.reviews-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+
+      425: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+
+      1250: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 2,
+      }
+    },
+    navigation: {
+      nextEl: '.reviews-button-next',
+      prevEl: '.reviews-button-prev',
     },
   });
